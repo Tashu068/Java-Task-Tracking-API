ca569f58f1096576e0975baf6c7879f1827bbda7
Tashu068 <112809362+Tashu068@users.noreply.github.com>
2023-08-04 20:54:29 +0530

    Create TaskService.java

---
+++ source/TaskService.java
@@ -0,0 +1,40 @@
+package com.example.tasktracker.service;
+
+import com.example.tasktracker.model.Task;
+import com.example.tasktracker.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(String id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(String id) {
+        taskRepository.deleteById(id);
+    }
+}
